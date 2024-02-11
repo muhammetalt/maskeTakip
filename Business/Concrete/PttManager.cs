2df74a5b842908bf3890542b73cd29d7df66ace9
muhammet <devopsdeveloper06@gmail.com>
2024-02-11 16:43:21 +0300

    maskeTakip

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,36 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PttManager:ISupplierService
+{
+    private IApplicantService _applicantService;//dependency Injection
+
+
+    public PttManager(IApplicantService applicantService)
+    {
+        _applicantService = applicantService;
+    }
+
+
+
+    public void GiveMask(Person person)
+    {
+       
+        if (_applicantService.CheckPerson(person))
+        {
+            Console.WriteLine(person.FirsName + " için maske verildi");
+        }
+        else
+        {
+            Console.WriteLine("Maske verilemedi");
+        }
+    }
+}
---
+++ workaround/Program.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Entities.Concrete;
+
+class Program
+{
+    private ISupplierService _supplierService;
+
+    
+    static void Main(string[] args)
+    {
+        
+         Person person1 = new Person();
+        person1.FirsName = "ENGİN";
+        person1.LastName = "DEMİROĞ";
+        person1.DateOfBirthYear = 1985;
+        person1.NationalIdentity = 1234;
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+     
+
+
+
+     
+    }
+
+    
+}
+
+
